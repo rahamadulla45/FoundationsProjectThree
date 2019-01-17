87501015810d79303dce0133d143f89e2d6a2959
Rahamadulla <rshafayath@gmail.com>
2019-01-17 15:41:42 +0300

    finish

--- components.py
+++ components.py
@@ -2,21 +2,32 @@
 class Person():
     def __init__(self, name, bio, age):
         # your code goes here!
-
+        self.name = name
+        self.bio = bio
+        self.age = age
 
 class Club():
     def __init__(self, name, description):
         # your code goes here!
-
+       self.name = name
+       self.description = description
+       self.members = []
+       self.president = None
 
     def assign_president(self, person):
         # your code goes here!
-
+        if person in self.members:
+            self.president = person
 
     def recruit_member(self, person):
         # your code goes here!
-
+        if person not in self.members:
+            self.members.append(person)
 
     def print_member_list(self):
         # your code goes here!
-
+        for member in self.members:
+            if member == self.president:
+                 print("- %s (%s years old) - %s" % (member.name, member.age, member.bio))
+            else:
+                 print("- %s (%s years old) - %s" % (member.name, member.age, member.bio))
